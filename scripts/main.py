9005b8286f5b22299b308123de001c2be27aee5c
MakotoYoshigoe <s20C1133AD@s.chibakoudai.jp>
2021-12-29 00:36:33 +0900

    Update file

--- scripts/main.py
+++ scripts/main.py
@@ -7,20 +7,22 @@ class Translate():
     def __init__(self, window, langs):
         self.window = window
         self.langs = langs
-        self.lang_label = [tk.Label(text='', anchor=tk.W) for i in langs]
-        self.result = [tk.Label(text='', anchor=tk.W) for i in langs]
+        self.lang_label = [tk.Label(text='', anchor=tk.W) for i in self.langs]
+        self.result = [tk.Label(text='', anchor=tk.W) for i in self.langs]
         self.pronounce = [tk.Button(self.window, text=f'{key}再生') for key, value in langs.items()]
         self.play_sound = rospy.ServiceProxy('play_sound', Str)
         self.translator = rospy.ServiceProxy('translate', Str)
         self.create_quit_btn()
         self.create_textbox()
 
+    #終了ボタン作成
     def create_quit_btn(self):
         x, y = 600, 400
         quit_btn = tk.Button(self.window, text='Quit')
         quit_btn['command'] = self.window.destroy
         quit_btn.place(x=x, y=y+40)
     
+    #テキストボックス作成
     def create_textbox(self):
         x, y = 150, 40
         label = tk.Label(text='Origin')
@@ -30,25 +32,31 @@ class Translate():
         trans_btn = tk.Button(self.window, text='Translate', command=self.trans_btn_cb)
         trans_btn.place(x=x+410, y=y-4)
     
+    #翻訳ボタンが押されたときの動作
     def trans_btn_cb(self):
         i = 0
         adjuster = tk.Label(text='')
         adjuster.grid(row=0, column=0, padx = 30, pady = 30)
         rospy.loginfo(self.txt.get())
         for key, value in self.langs.items():
-            txt = self.translator(f'{self.txt.get()}, {value}')
+            txt = self.translator(f'{self.txt.get()}, {value}') #翻訳された文を受け取る
+            #言語を表示
             self.lang_label[i]['text'] = key
             self.lang_label[i].grid(row = i+100, column = 3, padx = 2, pady = 2)
+            #翻訳した文を表示
             self.result[i]['text'] = txt.strOut
             self.result[i].grid(row = i+100, column = 4, padx = 2, pady = 2)
+            #再生ボタン作成
             self.pronounce[i].grid(row = i+100, column = 5,padx = 2, pady = 2)
             self.pronounce[i].bind("<ButtonPress>", self.pron_btn_cb)
             i += 1
     
+    #再生ボタンが押されたときの動作
     def pron_btn_cb(self, event):
-        lang = event.widget.cget("text").replace('再生', '')
+        lang = event.widget.cget("text").replace('再生', '') #音声を再生するサーバーに指示を出す
         result = self.play_sound(self.langs[lang])
 
+#メイン処理
 def main():
     rospy.init_node('main')
     wait_srv = ['translate', 'play_sound']
